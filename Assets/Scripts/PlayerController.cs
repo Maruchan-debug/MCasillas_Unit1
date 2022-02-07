803f1972d2e1fdd5e939b9cc5ec5f58fabcc3fbb
Maruchan-debug <78717496+Maruchan-debug@users.noreply.github.com>
2022-02-06 20:21:23 -0800

    Unit 1 Lesson B RE:
    Updated Code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Time.deltaTime);
+        //Vector3.forward ---> (0,0,1)
+        //(0,0,1) * Time.deltaTime = (0,0,.016) * = (0,0,.16)
         transform.Translate(Vector3.forward * Time.deltaTime * 10);
     }
 }
